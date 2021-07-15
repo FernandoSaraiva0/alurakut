30a64f2c0a1f71b798e4979df19895a5cf7eab79
CTBJ-Tecnico <ctbj-tecnico@pop-os.localdomain>
2021-07-15 16:43:08 -0300

    alurakut base

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,32 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutComons'
 
 const GlobalStyle = createGlobalStyle`
-  body {
+
+  /* Reset CSS Necolas Reset (pesquisar)*/
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  body {
+    background-color: #D9E6F6;
+    font-family: sans-serif;
+  }
+
+  #_next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    border-radius: 8px;
+  }
+  ${AlurakutStyles}
 `
 
 const theme = {
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,70 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutComons'
+import {ProfileRelationsBoxWrapper} from '../src/components/ProfileRelations'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
+function ProfileSidebar(propriedades) {
+  return(
+      <Box>
+        <img src={`https://github.com/${propriedades.User}.png`} /> 
+      </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+
+    const User = "fernandosaraiva0";
+    const favoriteHumans = [
+      'peas',
+      'omariosouto',
+      'juunegreiros',
+      'egermano'
+    ]
+    return (
+      <>
+        <AlurakutMenu/>
+        <MainGrid>
+          {/* <Box style="grid-area:profileArea;">  Não dá certo*/}
+          <div className= "profileArea" style={{ gridArea: 'profileArea' }}>
+            <ProfileSidebar User={User}/>
+          </div>
+
+          <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+            <Box>
+              <h2 className="title">
+                Bem-vindo(a)
+              </h2>
+
+              <OrkutNostalgicIconSet />
+            </Box>
+          </div>
+
+          <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+           <ProfileRelationsBoxWrapper>
+              <h2 className="smallTitle">
+                Pessoas da Comuniade ({favoriteHumans.length})
+              </h2>
+              
+              <ul>
+                {favoriteHumans.map((itemAtual) => {
+                  return(
+                  <li>
+                      <a href={`/users/${itemAtual}`} key={itemAtual}>
+                        <img src={`https://github.com/${itemAtual}.png`} />
+                        <span>{itemAtual}</span>
+                      </a>
+                  </li>
+                  )
+                })}
+              </ul>
+            </ProfileRelationsBoxWrapper>
+
+            <Box>
+              comunidade
+            </Box>
+          </div>
+        </MainGrid>
+    </>
+    )
 }
