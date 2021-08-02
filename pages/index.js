cc8aa85d15a06a024ffdea13e3f0053ddce29709
Fernando <fernandotiweb@gmail.com>
2021-08-02 16:06:36 -0300

    Community Box and adding in form
    Criação a aba comunidades e tornando dinamico o form para captar dados do usuário e criar novas comunidades.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import {AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet} from '../src/lib/AlurakutComons'
@@ -26,6 +27,10 @@ function ProfileSidebar(propriedades) {
 export default function Home() {
 
     const User = "fernandosaraiva0";
+    const [comunidades, setComunidades] = React.useState([{
+      title: 'Eu odeio acordar cedo',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+    }]);
     const favoriteHumans = [
       'peas',
       'omariosouto',
@@ -49,9 +54,53 @@ export default function Home() {
 
               <OrkutNostalgicIconSet />
             </Box>
+              <Box>
+                <h2 className="subTitle">O que você deseja fazer?</h2>
+                  <form onSubmit={function handleCriaComunidade(e) {
+                    e.preventDefault();
+
+                    const comunidadesAtualizadas = [...comunidades, 'AluraStars'];
+                    setComunidades(comunidadesAtualizadas)
+                  }}>
+                    <div>
+                      <input
+                        placeholder="Qual vai ser o nome da sua comunidade?"
+                        name="title"
+                        aria-label="Qual vai ser o nome da sua comunidade?"
+                        type="text"
+                        />
+                    </div>
+                    <div>
+                      <input
+                        placeholder="Coloque uma URL para usarmos de capa"
+                        name="image"
+                        aria-label="Coloque uma URL para usarmos de capa"
+                      />
+                    </div>
+
+                    <button>
+                      Criar comunidade
+                    </button>
+                   </form>
+            </Box>
           </div>
 
           <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+	            <ul>
+	              {comunidades.map((itemAtual) =>{
+		              return(
+                    <li>
+				              <a href={`/users/${itemAtual.title}`} key={itemAtual}>
+					              <img src={itemAtual.image} />
+					              <span>{itemAtual.title}</span>
+				              </a>
+                    </li>
+                  )
+                })}  
+	           </ul>
+          </ProfileRelationsBoxWrapper>
+
            <ProfileRelationsBoxWrapper>
               <h2 className="smallTitle">
                 Pessoas da Comuniade ({favoriteHumans.length})
@@ -71,9 +120,6 @@ export default function Home() {
               </ul>
             </ProfileRelationsBoxWrapper>
 
-            <Box>
-              comunidade
-            </Box>
           </div>
         </MainGrid>
     </>
